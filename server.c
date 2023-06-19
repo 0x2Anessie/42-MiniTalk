34435161646d7196af649c0b49e8a34d71aef7a7
Nessie <114988574+0x2Anessie@users.noreply.github.com>
2023-06-19 19:43:06 +0200

    Update server.c

--- server.c
+++ server.c
@@ -1,4 +1,44 @@
+#include "minitalk.h"
+
+static void joli_talk(int pid)
+{
+    ft_printf("%s                                                                                                                                                    %s\n");
+    ft_printf("%s██     ██ ███████ ██       ██████  ██████  ███    ███ ███████     ████████  ██████      ███    ███ ██ ███    ██ ██ ████████  █████  ██      ██   ██ %s\n");
+    ft_printf("%s██     ██ ██      ██      ██      ██    ██ ████  ████ ██             ██    ██    ██     ████  ████ ██ ████   ██ ██    ██    ██   ██ ██      ██  ██  %s\n");
+    ft_printf("%s██  █  ██ █████   ██      ██      ██    ██ ██ ████ ██ █████          ██    ██    ██     ██ ████ ██ ██ ██ ██  ██ ██    ██    ███████ ██      █████   %s\n");
+    ft_printf("%s██ ███ ██ ██      ██      ██      ██    ██ ██  ██  ██ ██             ██    ██    ██     ██  ██  ██ ██ ██  ██ ██ ██    ██    ██   ██ ██      ██  ██  %s\n");
+    ft_printf("%s ███ ███  ███████ ███████  ██████  ██████  ██      ██ ███████        ██     ██████      ██      ██ ██ ██   ████ ██    ██    ██   ██ ███████ ██   ██ %s\n");
+    ft_printf("%s                                                                                                                                                    %s\n");
+    ft_printf("PID: %d\n", pid);
+}
+
+static void gestionnaire_signal(int signal)
+{
+    static int bit; // static pour que la variable conserve sa valeur
+    static int i; // entre les différents appels de la fonction
+
+    if (signal == SIGUSR1 || signal == SIGUSR2)
+        i |= (0x01 << bit); // décale cette valeur binaire vers la gauche de "bit" positions
+    bit++;
+    if (bit == 8) // assez de bit pour composer un caractère à afficher
+    {
+        ft_printf("%c", i);
+        bit = 0;
+        i = 0;
+    }
+}
+
 int	main(void)
 {
-	
+	int pid;
+
+    pid = getpid();
+    joli_talk(pid);
+    while (1) // boucle infinie pour pas avoir a relancer le serveur
+        {
+            signal(SIGUSR1, gestionnaire_signal); // signal() définit le gestionnaire invoquer
+            signal(SIGUSR2, gestionnaire_signal); // en cas de réception d'un signal particulier 
+            pause(); // force le processus à s'endormir jusqu'à ce qu'un nouveau signal soit reçu
+        }
+    return (0);
 }
