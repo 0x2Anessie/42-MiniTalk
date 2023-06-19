695c3bc73c08c5488a4e083e5842670961225565
Nessie <114988574+0x2Anessie@users.noreply.github.com>
2023-06-19 19:42:09 +0200

    Update client.c

--- client.c
+++ client.c
@@ -1,4 +1,15 @@
+#include "minitalk.h"
+
+int f(char *str)
+{
+
+}
+
 int	main(int argc, char **argv)
 {
-	
+	char    *str;
+
+    str = argv[1];
+    if (ft_error(str) == 1)
+        return (0);
 }
