3440ff499b0651796458f3c828fcd4e99245bdbd
Nessie <114988574+0x2Anessie@users.noreply.github.com>
2023-06-26 17:10:03 +0200

    Add files via upload

---
+++ libft/ft_atoi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acatusse <acatusse@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/23 15:56:45 by acatusse          #+#    #+#             */
+/*   Updated: 2022/12/07 15:36:30 by acatusse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* Converti les chiffres d'une string en int */
+
+#include "libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int	num;
+	int	sign;
+
+	num = 0;
+	sign = 1;
+	while ((*nptr >= 9 && *nptr <= 13) || *nptr == 32)
+		nptr++;
+	if (*nptr == '-')
+		sign = -1;
+	if (*nptr == '-' || *nptr == '+')
+		nptr++;
+	while (*nptr >= '0' && *nptr <= '9')
+		num = num * 10 + *nptr++ - '0';
+	return (num * sign);
+}
