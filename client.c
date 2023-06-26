247c6916a61869f1f6d9dbabe63c757d39b0dbaf
Nessie <114988574+0x2Anessie@users.noreply.github.com>
2023-06-26 17:07:36 +0200

    Update client.c

--- client.c
+++ client.c
@@ -1,15 +1,31 @@
-#include "minitalk.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include "libft/libft.h"
 
-int f(char *str)
+static int ft_error(int argc, char **argv)
 {
+    if (argc != 3)
+        return (1);
+    return (0);
+    if (argv);
+}
 
+static void send_to_server(int pid, char *str)
+{
+    int bit;
+
+    kill(pid, SIGUSR1); // permet d'envoyer un signal a un pid
+    kill(pid, SIGUSR2);
 }
 
 int	main(int argc, char **argv)
 {
 	char    *str;
+    int pid;
 
-    str = argv[1];
-    if (ft_error(str) == 1)
-        return (0);
+    if (ft_error(argc, argv))
+        return (1);
+    str = argv[2];
+    pid = ft_atoi(argv[1]);
 }
