c2d8bcccb8ad8d5506e4dfb6aa4c8398927899e8
Nessie <114988574+0x2Anessie@users.noreply.github.com>
2023-06-30 17:30:52 +0200

    Update server.c

--- server.c
+++ server.c
@@ -3,45 +3,40 @@
 #include <signal.h>
 #include "libft/libft.h"
 
-static void joli_talk(int pid)
+static void	joli_talk(int pid)
 {
-    ft_printf("%s                                                                                                                                                    %s\n");
-    ft_printf("%s██     ██ ███████ ██       ██████  ██████  ███    ███ ███████     ████████  ██████      ███    ███ ██ ███    ██ ██ ████████  █████  ██      ██   ██ %s\n");
-    ft_printf("%s██     ██ ██      ██      ██      ██    ██ ████  ████ ██             ██    ██    ██     ████  ████ ██ ████   ██ ██    ██    ██   ██ ██      ██  ██  %s\n");
-    ft_printf("%s██  █  ██ █████   ██      ██      ██    ██ ██ ████ ██ █████          ██    ██    ██     ██ ████ ██ ██ ██ ██  ██ ██    ██    ███████ ██      █████   %s\n");
-    ft_printf("%s██ ███ ██ ██      ██      ██      ██    ██ ██  ██  ██ ██             ██    ██    ██     ██  ██  ██ ██ ██  ██ ██ ██    ██    ██   ██ ██      ██  ██  %s\n");
-    ft_printf("%s ███ ███  ███████ ███████  ██████  ██████  ██      ██ ███████        ██     ██████      ██      ██ ██ ██   ████ ██    ██    ██   ██ ███████ ██   ██ %s\n");
-    ft_printf("%s                                                                                                                                                    %s\n");
-    ft_printf("PID: %d\n", pid);
+	ft_printf("Blip Bloup...\n");
+	ft_printf("Voilà mon PID: %d\n", pid); // affiche le pid
+	ft_printf("Tu peux écrire ton message bg :)\n");
 }
 
-static void gestionnaire_signal(int signal)
+static void	gestionnaire_signal(int signal)
 {
-    static int bit; // static pour que la variable conserve sa valeur
-    static int i; // entre les différents appels de la fonction
+	static int	bit; // static pour que la variable conserve sa valeur
+	static int	i; // entre les différents appels de la fonction
 
-    if (signal == SIGUSR1 || signal == SIGUSR2)
-        i |= (0x01 << bit); // décale cette valeur binaire vers la gauche de "bit" positions
-    bit++;
-    if (bit == 8) // assez de bit pour composer un caractère à afficher
-    {
-        ft_printf("%c", i);
-        bit = 0;
-        i = 0;
-    }
+	if (signal == SIGUSR1 || signal == SIGUSR2)
+		i |= (0x01 << bit); // décale cette valeur binaire vers la gauche de "bit" positions
+	bit++;
+	if (bit == 8) // assez de bit pour composer un caractère à afficher
+	{
+		ft_printf("%c", i);
+		bit = 0;
+		i = 0;
+	}
 }
 
 int	main(void)
 {
-	int pid;
+	int	pid;
 
-    pid = getpid();
-    joli_talk(pid);
-    while (1) // boucle infinie pour pas avoir a relancer le serveur
-        {
-            signal(SIGUSR1, gestionnaire_signal); // signal() définit le gestionnaire invoquer
-            signal(SIGUSR2, gestionnaire_signal); // en cas de réception d'un signal particulier 
-            pause(); // force le processus à s'endormir jusqu'à ce qu'un nouveau signal soit reçu
-        }
-    return (0);
+	pid = getpid();
+	joli_talk(pid);
+	while (1) // boucle infinie pour pas avoir a relancer le serveur
+		{
+			signal(SIGUSR1, gestionnaire_signal); // signal() définit le gestionnaire invoquer
+			signal(SIGUSR2, gestionnaire_signal); // en cas de réception d'un signal particulier 
+			pause(); // force le processus à s'endormir jusqu'à ce qu'un nouveau signal soit reçu
+		}
+	return (0);
 }
