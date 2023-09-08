6deceb667a660829edad627be4c287082ec0b639
Nessie <114988574+0x2Anessie@users.noreply.github.com>
2023-09-08 18:46:03 +0200

    Update server.c

--- server.c
+++ server.c
@@ -7,10 +7,10 @@ static void	joli_talk(int pid)
 {
 	ft_printf("Blip Bloup...\n");
 	ft_printf("Voilà mon PID: %d\n", pid); // affiche le pid
-	ft_printf("Tu peux écrire ton message bg :)\n");
+	ft_printf("Tu peux écrire ton message :)\n");
 }
 
-static void	gestionnaire_signal(int signal)
+static void	receive_and_print_signal(int signal)
 {
 	static int	bit; // static pour que la variable conserve sa valeur
 	static int	i; // entre les différents appels de la fonction
@@ -28,14 +28,11 @@ static void	gestionnaire_signal(int signal)
 
 int	main(void)
 {
-	int	pid;
-
-	pid = getpid();
-	joli_talk(pid);
+	joli_talk(getpid());
 	while (1) // boucle infinie pour pas avoir a relancer le serveur
 		{
-			signal(SIGUSR1, gestionnaire_signal); // signal() définit le gestionnaire invoquer
-			signal(SIGUSR2, gestionnaire_signal); // en cas de réception d'un signal particulier 
+			signal(SIGUSR1, receive_and_print_signal); // signal() définit le gestionnaire invoquer
+			signal(SIGUSR2, receive_and_print_signal); // en cas de réception d'un signal particulier 
 			pause(); // force le processus à s'endormir jusqu'à ce qu'un nouveau signal soit reçu
 		}
 	return (0);
