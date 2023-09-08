2442e69ebb749169e076922216295c6062c19b22
Nessie <114988574+0x2Anessie@users.noreply.github.com>
2023-09-08 18:46:49 +0200

    Update client.c

--- client.c
+++ client.c
@@ -3,29 +3,53 @@
 #include <signal.h>
 #include "libft/libft.h"
 
-static int ft_error(int argc, char **argv)
-{
-    if (argc != 3)
-        return (1);
-    return (0);
-    if (argv);
-}
+// A chaque nouveau caractere trouve par i dans argv[2] :
+// On appelle la fonction convert_and_send pour convertir chaque
+// octet en bit et envoyer leur signal au pid du serveur.
+// La fonction traite donc 8 octets a chaque fois qu'elle est appelee
+// et utilise la fonction kill, qui envoie un signal a un serveur grace
+// a son PID.
+// Le type de signal est specifie par SIGUSR1 et 2, 
 
-static void send_to_server(int pid, char *str)
+static int convert_and_send_signal(int pid, char str)
 {
+    int i;
     int bit;
 
-    kill(pid, SIGUSR1); // permet d'envoyer un signal a un pid
-    kill(pid, SIGUSR2);
+	i = 0;
+	while (i < 8)
+	{
+		bit = (str >> i) & 1;
+		if (bit)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(200);
+		i++;
+	}
+	return (1);
 }
 
 int	main(int argc, char **argv)
 {
-	char    *str;
+    int i;
     int pid;
 
-    if (ft_error(argc, argv))
-        return (1);
-    str = argv[2];
+    if (argc != 3)
+    {
+        ft_printf("MAUVAIS FORMAT: ");
+        ft_printf("Essaye en mettant ");
+        ft_printf("<./client> <le PID du server> <le message a transmettre>\n");
+        return (0);
+    }
+    i = 0;
     pid = ft_atoi(argv[1]);
+    while (argv[2][i])
+    {
+        convert_and_send(pid, argv[2][i]);
+        i++;
+    }
+    if (argv[2][i] == '\0')
+		ft_printf("Message envoye :)");
+    return(0);
 }
