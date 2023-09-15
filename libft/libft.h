03c62a6923433388c6e8c1cb2ef626f412fa4c40
Nessie <114988574+0x2Anessie@users.noreply.github.com>
2023-09-15 14:45:57 +0200

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -8,25 +8,24 @@
 # include <stdarg.h>
 # include <limits.h>
 
-int	ft_formats(va_list args, const char format);
-int	ft_printf(const char *str, ...);
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000000000
+# endif
 
-int	print_single_char(int c);
-int	print_multiple_chars(char *str);
-int	print_pointer(unsigned long long pointer);
-int	print_numbers(int nb);
+int		ft_formats(va_list args, const char format);
+int		ft_printf(const char *str, ...);
 
-int	print_unsigned_numbers(unsigned int nb);
-int	print_hexa_min(unsigned long long nb);
-int	print_hexa_maj(unsigned int nb);
-int	print_percent(void);
+int		print_single_char(int c);
+int		print_multiple_chars(char *str);
+int		print_pointer(unsigned long long pointer);
+int		print_numbers(int nb);
+
+int		print_unsigned_numbers(unsigned int nb);
+int		print_hexa_min(unsigned long long nb);
+int		print_hexa_maj(unsigned int nb);
+int		print_percent(void);
 
 int		ft_atoi(const char *nptr);
-
-void	ft_putendl_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *s, int fd);
 
 size_t	ft_strlen(const char *s);
 
