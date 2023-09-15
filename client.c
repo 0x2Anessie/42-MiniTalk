4b6569fc2cd9709418a9a8b2b35215339aa8176a
Nessie <114988574+0x2Anessie@users.noreply.github.com>
2023-09-15 14:42:42 +0200

    Update client.c

--- client.c
+++ client.c
@@ -5,60 +5,60 @@
 #include <string.h>
 #include "libft/libft.h"
 
-int g_signal = 0;
+int	g_signal = 0;
 
-static  void receive_signal(int signal)
+static void	receive_signal(int signal)
 {
-    g_signal = 1;
+	g_signal = 1;
 }
 
-static int ft_error(const char *msg)
+static int	ft_error(const char *msg)
 {
-    ft_printf("%s\n", msg);
-    return (1);
+	ft_printf("%s\n", msg);
+	return (1);
 }
 
-static void send_char(int pid, char c)
+static void	send_char(int pid, char c)
 {
-	int bit;
-    int i;
+	int	bit;
+	int	i;
 
 	i = 0;
-    while (i < 8)
-    {
-        bit = (c >> i) & 1;
-        if (bit)
-            kill(pid, SIGUSR1);
-        else
-            kill(pid, SIGUSR2);
-        while (g_signal != 1)
-            ;
-        g_signal = 0;
-        i++;
-    }
+	while (i < 8)
+	{
+		bit = (c >> i) & 1;
+		if (bit)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		while (g_signal != 1)
+			;
+		g_signal = 0;
+		i++;
+	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	int i;
-	int pid;
-    int len;
-	char *message;
+	int		i;
+	int		pid;
+	int		len;
+	char	*message;
 
-    if (argc != 3)
-		return ft_error("Format incorrect: ./client <PID du serveur> <message>");
+	if (argc != 3)
+		return (ft_error("Format incorrect: ./client <PID> <msg>"));
 	i = 0;
 	pid = ft_atoi(argv[1]);
-    if (pid < 0)
-        return (0);
+	if (pid < 0)
+		return (0);
 	message = argv[2];
 	len = ft_strlen(message);
-    signal(SIGUSR1, receive_signal);
-    while (i < len)
-    {
-        send_char(pid, message[i]);
-        i++;
-    }
-    send_char(pid, '\0');
-    return (0);
+	signal(SIGUSR1, receive_signal);
+	while (i < len)
+	{
+		send_char(pid, message[i]);
+		i++;
+	}
+	send_char(pid, '\0');
+	return (0);
 }
